17992793cbd598a0436f56815345ffff41e850d3
Andrew Minh Nguyen <amqdnguyen@gmail.com>
2024-12-02 03:11:57 -0600

    Portfolio: v1.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1518507774"
+    serviceWorkerVersion: "2144228965"
   }
 });
